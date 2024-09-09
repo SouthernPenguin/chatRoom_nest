b61e7d16a600e5baaa076a7cafa09be26ae5d196
SouthernPenguin <940583551@qq.com>
2024-09-09 16:23:42 +0800

    feat:通用方法

---
+++ src/utils/db.help.ts
@@ -0,0 +1,31 @@
+import { SelectQueryBuilder } from 'typeorm';
+
+export const conditionUtilsLike = <T>(
+  queryBuilder: SelectQueryBuilder<T>,
+  arr: any[],
+  entityName: string,
+) => {
+  arr.forEach((item) => {
+    if (item.value) {
+      queryBuilder.andWhere(`${entityName}.${item.field} Like:${item.field}`, {
+        [item.field]: `%${item.value}%`,
+      });
+    }
+  });
+  return queryBuilder;
+};
+
+export const conditionUtilsSelect = <T>(
+  queryBuilder: SelectQueryBuilder<T>,
+  arr: any[],
+  entityName: string,
+) => {
+  arr.forEach((item) => {
+    if (item.value) {
+      queryBuilder.andWhere(`${entityName}.${item.field} = :${item.field}`, {
+        [item.field]: item.value,
+      });
+    }
+  });
+  return queryBuilder;
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,4 @@
-import { UnauthorizedException } from '@nestjs/common';
-
-type token = { username: string; id: number };
+type token = { username: string; id: number; roleId: number[] };
 export const getTokenUser = async (req: Request): Promise<token> => {
   if (req && 'user' in req) {
     return req['user'] as token;
@@ -15,4 +13,28 @@ export const formatFileSize = (bytes: number) => {
   const i = parseInt(Math.floor(Math.log(bytes) / Math.log(k)) as any);
 
   return Math.round(100 * (bytes / Math.pow(k, i))) / 100 + ' ' + sizes[i];
+};
+
+/**
+ * 生成树状图
+ * @param list 数组
+ * @param parentId 分类id
+ * @returns Array
+ */
+export const menuTree = (
+  list: any[],
+  parentId: number | null,
+): any[] | string => {
+  if (list.length < 0) return '请传入数组';
+  const returnList: any[] = [];
+  list.forEach((item) => {
+    if (
+      item.parent_id === parentId &&
+      (item.isDeleted === 0 || !item.isDeleted)
+    ) {
+      item.children = menuTree(list, item.id);
+      returnList.push(item);
+    }
+  });
+  return returnList;
 };
