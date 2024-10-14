0cec53fe15cc9a7010355c29ea2c5c8890e44ee7
SouthernPenguin <940583551@qq.com>
2024-10-14 17:03:12 +0800

    format(custom): 代码格式化

--- src/ststem-user/ststem-user.service.ts
+++ src/ststem-user/ststem-user.service.ts
@@ -1,8 +1,4 @@
-import {
-  Injectable,
-  ForbiddenException,
-  BadRequestException,
-} from '@nestjs/common';
+import { Injectable, ForbiddenException, BadRequestException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { GetSystemUserDto } from './dto/get-system-user.dto';
@@ -12,10 +8,7 @@ import { SystemUser } from './entities/ststem-user.entity';
 import { CreateSystemUserDto } from './dto/create-ststem-user.dto';
 import { conditionUtilsSelect } from 'src/utils/db.help';
 import { Menu } from 'src/menu/entities/menu.entity';
-import {
-  ChangeSystemUserPasswordDto,
-  UpdateSystemUserDto,
-} from './dto/update-ststem-user.dto';
+import { ChangeSystemUserPasswordDto, UpdateSystemUserDto } from './dto/update-ststem-user.dto';
 
 @Injectable()
 export class SystemUserService {
@@ -26,14 +19,10 @@ export class SystemUserService {
   ) {}
 
   async create(createSystemUserDto: CreateSystemUserDto) {
-    const list = await this.rolesService.returnRolesServiceEntities(
-      createSystemUserDto.roleIds,
-    );
+    const list = await this.rolesService.returnRolesServiceEntities(createSystemUserDto.roleIds);
     createSystemUserDto.roles = list;
     // 加密
-    createSystemUserDto.password = encrypt(
-      createSystemUserDto.password ? createSystemUserDto.password : '123456',
-    );
+    createSystemUserDto.password = encrypt(createSystemUserDto.password ? createSystemUserDto.password : '123456');
     const res = await this.systemUserRepository.save(createSystemUserDto);
     delete res.roleIds;
     return res;
@@ -42,14 +31,9 @@ export class SystemUserService {
   async findAll(query: GetSystemUserDto) {
     const { page, limit, name } = query;
     const conditions = [{ field: 'name', value: name }];
-    const queryBuilder =
-      await this.systemUserRepository.createQueryBuilder('system_user');
+    const queryBuilder = await this.systemUserRepository.createQueryBuilder('system_user');
 
-    const newQueryBuilder = conditionUtilsSelect<SystemUser>(
-      queryBuilder,
-      conditions,
-      'system_user',
-    );
+    const newQueryBuilder = conditionUtilsSelect<SystemUser>(queryBuilder, conditions, 'system_user');
     const count = await newQueryBuilder.getCount();
     const content = await newQueryBuilder
       .leftJoinAndSelect('system_user.roles', 'role')
@@ -77,13 +61,13 @@ export class SystemUserService {
       throw new ForbiddenException('用户不存在');
     }
     const menus: Menu[] = [];
-    res.roles.map((item) => {
+    res.roles.map(item => {
       if (item.menus.length) {
         menus.push(...item.menus);
       }
     });
     // 角色
-    const roles: { id: number; name: string }[] = res.roles.map((item) => {
+    const roles: { id: number; name: string }[] = res.roles.map(item => {
       return {
         id: item.id,
         name: item.name,
@@ -91,7 +75,7 @@ export class SystemUserService {
     });
     // 按钮权限
     const buttonList: string[] = [];
-    menus.map((item) => {
+    menus.map(item => {
       if (item.nodeType === 3) {
         buttonList.push(item.menuCode);
       }
@@ -112,18 +96,13 @@ export class SystemUserService {
     if (!res.id) {
       throw new ForbiddenException('用户不存在');
     }
-    const list = await this.rolesService.returnRolesServiceEntities(
-      updateSystemUserDto.roleIds,
-    );
+    const list = await this.rolesService.returnRolesServiceEntities(updateSystemUserDto.roleIds);
     updateSystemUserDto.roles = list;
     const tes = await this.systemUserRepository.merge(res, updateSystemUserDto);
     return this.systemUserRepository.save(tes);
   }
 
-  async updatePassword(
-    id: number,
-    updateSystemUserDto: ChangeSystemUserPasswordDto,
-  ) {
+  async updatePassword(id: number, updateSystemUserDto: ChangeSystemUserPasswordDto) {
     const res = await this.findOne(id);
     if (!res.id) {
       throw new ForbiddenException('用户不存在');
