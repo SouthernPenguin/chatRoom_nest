68a8a21a23cce2d0a11fe40d2256077e2c193472
SouthernPenguin <940583551@qq.com>
2024-10-24 16:33:02 +0800

    fix(custom): 引入实体找不到

--- ormconfig.ts
+++ ormconfig.ts
@@ -2,16 +2,16 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import redConfigFile from 'src/utils/redConfigFile';
 
-// import { FriendShip } from 'src/friend-ship/entities/friend-ship.entity';
-// import { Message } from 'src/message/entities/message.entity';
-// import { User } from 'src/user/entities/user.entity';
-// import { Notice } from 'src/notification/entities/notice.entity';
-// import { GroupChat } from 'src/group-chat/entities/group-chat.entity';
-// import { GroupMessage } from 'src/group-message/entities/group-message.entity';
-// import { GroupChatUser } from 'src/group-chat/entities/group-chat-user.entity';
-// import { Menu } from 'src/menu/entities/menu.entity';
-// import { Role } from 'src/role/entities/role.entity';
-// import { SystemUser } from 'src/ststem-user/entities/ststem-user.entity';
+import { FriendShip } from 'src/friend-ship/entities/friend-ship.entity';
+import { Message } from 'src/message/entities/message.entity';
+import { User } from 'src/user/entities/user.entity';
+import { Notice } from 'src/notification/entities/notice.entity';
+import { GroupChat } from 'src/group-chat/entities/group-chat.entity';
+import { GroupMessage } from 'src/group-message/entities/group-message.entity';
+import { GroupChatUser } from 'src/group-chat/entities/group-chat-user.entity';
+import { Menu } from 'src/menu/entities/menu.entity';
+import { Role } from 'src/role/entities/role.entity';
+import { SystemUser } from 'src/ststem-user/entities/ststem-user.entity';
 
 const mySqlConfig: any = redConfigFile();
 
@@ -23,9 +23,9 @@ export const connectionParams = {
   password: mySqlConfig.db.mysql.password, // 数据库密码
   database: mySqlConfig.db.mysql.database, // 库名
   name: 'default',
-  // User, FriendShip, Message, Menu, Notice, Role, SystemUser, GroupChat, GroupMessage, GroupChatUser
-  entities: [__dirname + 'src/**/entities/*.entity{.ts,.js}'], // 导入的实体(数据库模型)
-  synchronize: false,
+  entities: [User, FriendShip, Message, Menu, Notice, Role, SystemUser, GroupChat, GroupMessage, GroupChatUser], // 导入的实体(数据库模型)
+  // entities: [__dirname + 'src/**/entities/**.entity{.ts,.js}'], // 导入的实体(数据库模型)
+  synchronize: true,
   // 日志等级
   logging: true, //['query', 'error'],
 
