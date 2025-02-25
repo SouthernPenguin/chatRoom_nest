451214474ddc24fd0f13a13d08e08a74fe5dbd4e
SouthernPenguin <940583551@qq.com>
2025-02-25 16:01:50 +0800

    format(custom): 格式化代码

--- src/utils/redConfigFile.ts
+++ src/utils/redConfigFile.ts
@@ -5,16 +5,10 @@ import * as _ from 'lodash';
 
 const redConfigFile = () => {
   const YML_CONFIG_FILE = 'config.yml';
-  const YML_NODE_ENV = `config.${
-    process.env.NODE_ENV === 'development' ? 'development' : 'production'
-  }.yml`;
-
-  const envConfig = yaml.load(
-    fs.readFileSync(path.join('config', YML_NODE_ENV), 'utf-8'),
-  );
-  const config = yaml.load(
-    fs.readFileSync(path.join('config', YML_CONFIG_FILE), 'utf-8'),
-  );
+  const YML_NODE_ENV = `config.${process.env.NODE_ENV === 'development' ? 'development' : 'production'}.yml`;
+  console.log('YML_NODE_ENV11111111', YML_NODE_ENV);
+  const envConfig = yaml.load(fs.readFileSync(path.join('config', YML_NODE_ENV), 'utf-8'));
+  const config = yaml.load(fs.readFileSync(path.join('config', YML_CONFIG_FILE), 'utf-8'));
   // 合并配置
   return _.merge(config, envConfig);
 };
